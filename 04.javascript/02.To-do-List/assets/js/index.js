1e918a7fdcee9935052705a2271c9c16ad6f57cd
William-MS <williamsartinidev@outlook.com>
2022-06-22 19:23:31 -0300

    adicionado o contador estilizado

---
+++ 04.javascript/02.To-do-List/assets/js/index.js
@@ -0,0 +1,10 @@
+var novaTarefa = document.getElementById('novaTarefa');
+var lista = document.getElementById("lista");
+
+function adicionar(){
+    // if(novaTarefa.value != " "){
+    // }
+
+    lista.insertAdjacentElement('beforeend', h2);
+    // lista.lastChild.innerHTML = novaTarefa.value;
+}
